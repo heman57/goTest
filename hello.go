20a9397e460765a78b45f394adebf276136e6998
heman57 <info@nanoit.cz>
2019-01-24 15:39:47 +0100

    a

--- hello.go
+++ hello.go
@@ -1,9 +1,11 @@
 package main
+
 import "fmt"
+
 func main() {
-    
-    for index := 0; index < 10; index++ {
-        fmt.Println("hello world")    
- 
-    }
+
+	for index := 0; index < 10; index++ {
+		fmt.Println("hello world")
+		fmt.Println("hi")
+	}
 }
